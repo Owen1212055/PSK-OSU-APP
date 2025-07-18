2168516a41d75cc39440d129364e2d4caf451e1a
Owen <23108066+Owen1212055@users.noreply.github.com>
2025-07-18 10:55:55 -0400

    Properly show the commit hash on the version component display
    Previously it checked the bundled version number, which needs to be handled differently as it's properly packaged with the ipa it seems.

--- components/newui/VersionComponent.tsx
+++ components/newui/VersionComponent.tsx
@@ -13,7 +13,7 @@ export const VersionComponent: React.FC<VersionProperties> = ({ hasIcon = true }
     const styles = createStyles(theme);
 
     let styledVersion = Constants.expoConfig?.version;
-    if (Constants.expoConfig?.ios?.buildNumber === undefined) {
+    if (true) {
         const rawHash = Constants.expoConfig?.extra?.commitHash ?? '';
         const shortHash = rawHash.slice(0, 7);
         styledVersion += " | " + shortHash;
