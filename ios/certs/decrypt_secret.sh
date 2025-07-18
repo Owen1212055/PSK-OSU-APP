fa17888a8b239b56dad8b11b33f440879bccb87b
Owen1212055 <23108066+Owen1212055@users.noreply.github.com>
2025-07-17 23:32:32 -0400

    Resign the credential files because I am an idiot and forgot the passphrase

--- ios/certs/decrypt_secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERT_DECRYPT_KEY" --output dist.p12 dist.p12.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERT_DECRYPT_KEY" --output profile.mobileprovision profile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERT_DECRYPT_KEY" --output AuthKey.p8 AuthKey.p8.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERT_DECRYPT_KEY_2" --output ../../credentials.json credentials.json.gpg
